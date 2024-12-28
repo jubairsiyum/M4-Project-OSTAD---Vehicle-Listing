bcacad428ad097120d4d92bb7197597d83d5e3c7
jubairsiyum <jubairsiyum@gmail.com>
2024-12-28 22:44:08 +0600

    up

--- public/index.php
+++ public/index.php
@@ -1,26 +1,28 @@
 <?php
 include './views/header.php';
+$jsonData = file_get_contents('../data/vehicles.json');
+$vehicles = json_decode($jsonData, true);
 ?>
 
 
 <div class="container my-4">
     <h1>Vehicle Listing for OSTADs</h1>
-    <a href="./../public/views/add.php" class="btn btn-success mb-4">Add Vehicle</a>
+    <a href="./views/add.php" class="btn btn-success mb-4">Add Vehicle</a>
     <div class="row">
-                
+        <?php foreach ($vehicles as $vehicle): ?>
             <div class="col-md-4">
                 <div class="card">
-                    <img src="" class="card-img-top" style="height: 200px; object-fit: cover;">
+                    <img src="images/<?php echo htmlspecialchars($vehicle['image']); ?>" class="card-img-top" style="height: 200px; object-fit: cover;">
                     <div class="card-body">
-                        <h5 class="card-title"></h5> 
-                         <p class="card-text">Type: </p>
-                        <p class="card-text">Price: $</p>
-                        <a href="./views/edit.php?id=" class="btn btn-primary">Edit</a>
-                        <a href="./views/delete.php?id=" class="btn btn-danger">Delete</a>
+                        <h5 class="card-title"><?php echo htmlspecialchars($vehicle['name']); ?></h5>
+                        <p class="card-text">Type: <?php echo htmlspecialchars($vehicle['type']); ?></p>
+                        <p class="card-text">Price: $<?php echo number_format($vehicle['price'], 2); ?></p>
+                        <a href="./views/edit.php?id=<?php echo urlencode($vehicle['id']); ?>" class="btn btn-primary">Edit</a>
+                        <a href="./views/delete.php?id=<?php echo urlencode($vehicle['id']); ?>" class="btn btn-danger">Delete</a>
                     </div>
                 </div>
             </div>
-            
+        <?php endforeach; ?>
     </div>
 </div>
 
