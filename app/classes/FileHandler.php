d443f3dd365ffb3890a4f187c96b53c29c993804
jubairsiyum <jubairsiyum@gmail.com>
2024-12-28 22:27:20 +0600

    file handler up

--- app/classes/FileHandler.php
+++ app/classes/FileHandler.php
@@ -1,12 +1,17 @@
 <?php
 trait FileHandler {
-    private $filePath = "";
+    private $filePath = "data/vehicles.json";
 
     public function readFile() {
-        
-    }
+        if (!file_exists($this->filePath)) {
+            return [];
+        }
+        $json = file_get_contents($this->filePath);
+        return json_decode($json, true) ?? [];
+    }    
 
     public function writeFile($data) {
-        
+        $json = json_encode($data, JSON_PRETTY_PRINT);
+        file_put_contents($this->filePath, $json);
     }
 }
